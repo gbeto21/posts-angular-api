77befbcc8308d1a2023a4f2f020e36ffbd7fd831
Alberto González Balderas <albertoglezb@hotmail.com>
2020-09-05 10:09:09 -0500

    The debug error when adding a post was created.

--- controllers/post.js
+++ controllers/post.js
@@ -21,6 +21,7 @@ exports.createPost = (req, res, next) => {
       )
     })
     .catch(error => {
+      console.log(error);
       res.status(500).json({
         message: "Creating a post failed."
       })
